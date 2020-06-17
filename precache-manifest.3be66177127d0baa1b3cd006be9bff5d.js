c7d0d422773a5d1b7793d18cbeb9504bcb8e858c
bilalshoaib <bilalshoaib@erip.me>
2020-06-17 20:49:50 +0500

    Updates

---
+++ precache-manifest.3be66177127d0baa1b3cd006be9bff5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d62a024d7b9b5337532fc2665d4010",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a142062660573edf7283",
+    "url": "/monsters/static/css/main.46a67f14.chunk.css"
+  },
+  {
+    "revision": "d064ea85da7c2e271b4d",
+    "url": "/monsters/static/js/2.d9d4b753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.d9d4b753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a142062660573edf7283",
+    "url": "/monsters/static/js/main.7f33afb2.chunk.js"
+  },
+  {
+    "revision": "65f44f7ddd0ca668d8b6",
+    "url": "/monsters/static/js/runtime-main.8b835e01.js"
+  }
+]);
